1a7ed2bc36a9fe33e6343eaa1d1650e76127fcd8
vijay2904 <vijayrohit2904@gmail.com>
2025-07-12 14:46:13 +0530

    Add introductory post on Cloud Computing

--- src/components/mdx-component.tsx
+++ src/components/mdx-component.tsx
@@ -17,17 +17,23 @@ interface CodeProps extends React.HTMLAttributes<HTMLElement> {
 interface Components {
     code: React.FC<CodeProps>;
     Counter: typeof Counter;
-    h1:　React.FC<React.OlHTMLAttributes<HTMLHeadingElement>>;
+    h1: React.FC<React.HTMLAttributes<HTMLHeadingElement>>;
+    h2: React.FC<React.HTMLAttributes<HTMLHeadingElement>>;
     ol: React.FC<React.OlHTMLAttributes<HTMLOListElement>>;
-    ul: React.FC<React.OlHTMLAttributes<HTMLUListElement>>;
+    ul: React.FC<React.HTMLAttributes<HTMLUListElement>>;
+    li: React.FC<React.LiHTMLAttributes<HTMLLIElement>>;
+    p: React.FC<React.HTMLAttributes<HTMLParagraphElement>>;
 }
 
 const components: Components = {
     code: Code,
     Counter,
-    h1:　(props) => <h1 className="text-3xl underline underline-offset-8" {...props} />,
+    h1: (props) => <h1 className="text-3xl underline underline-offset-8 pb-5" {...props} />,
+    h2: (props) => <h2 className="text-2xl underline underline-offset-8 pb-2" {...props} />,
     ol: (props) => <ol className="list-decimal pl-4" {...props} />,
     ul: (props) => <ul className="list-disc" {...props} />,
+    li: (props) => <li className="p-1 m-1 decoration-dotted" {...props} />,
+    p: (props) => <p className="py-1 my-1" {...props} />,
 };
 
 export default function MDXContent(
